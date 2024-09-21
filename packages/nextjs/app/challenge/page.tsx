148148d36f6bed186c0b32beb47b6097fd45cab2
Anjana2002 <anjanaa628@gmail.com>
2024-09-21 20:59:50 +0530

    added link to create challenge

---
+++ packages/nextjs/app/challenge/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Challenge = () => {
+  return (
+    <div>
+      <button>Create Challenge</button>
+      <button>Join Challenge</button>
+    </div>
+  )
+}
+
+export default Challenge;
+4
