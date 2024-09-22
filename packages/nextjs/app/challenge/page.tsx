fd18517ecf7100496660cb2fba59a85e50af2bd0
4rjunc <arjunmundamani@gmail.com>
2024-09-22 10:11:47 +0530

    Fixed the buttons

--- packages/nextjs/app/challenge/page.tsx
+++ packages/nextjs/app/challenge/page.tsx
@@ -39,11 +39,11 @@ const Challenge = () => {
   const toggleVoteModal = () => setVoteModalOpen(!isVoteModalOpen);
 
   return (
-    <div className="flex justify-between px-4 items-center bg-[#e9d8a6] text-[#333333] h-screen -mt-20">
+    <div className="flex justify-center items-center bg-[#e9d8a6] text-[#333333] h-screen gap-2">
       {/* Create Challenge Button */}
       <button
         onClick={toggleCreateModal}
-        className="bg-blue-600 hover:bg-blue-700 text-white font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
+        className="bg-blue-600 hover:bg-blue-700 w-60 h-32  text-white font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
       >
         Create Challenge
       </button>
@@ -51,7 +51,7 @@ const Challenge = () => {
       {/* Join Challenge Button */}
       <button
         onClick={toggleJoinModal}
-        className="bg-green-600 hover:bg-green-700 text-white font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300 ml-20"
+        className="bg-green-600 hover:bg-green-700 text-white w-60   h-32 font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
       >
         Join Challenge
       </button>
@@ -59,7 +59,7 @@ const Challenge = () => {
       {/* Vote Button */}
       <button
         onClick={toggleVoteModal}
-        className="bg-red-600 hover:bg-red-700 text-white font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
+        className="bg-red-600 hover:bg-red-700 text-white font-bold w-60 h-32 text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
       >
         Vote
       </button>
